062733a99b6b611a36098763da981019e156db62
Peeradon <peerado300@gmail.com>
2025-11-22 16:18:07 +0700

    add save load json

--- taskManager.py
+++ taskManager.py
@@ -1,12 +1,17 @@
 import datetime as dt
 import uuid
 import os 
+import json
+import time
+
 ## class Job : ประกอบด้วย user_name, jobID, deadline
 
 state = True
-job_list = [{"name": "backup Server", "describe": "ทำการ....", "deadline": "2025-01-2025", "job_id" : 4456},
-                {"name": "checkSystem", "describe": "ทำการ....", "deadline": "2025-08-2026", "job_id" : 1240}
-                ]
+job_list = [
+    {"name": "backup Server", "describe": "ทำการ....", "deadline": "2025-01-15", "job_id": "4456", "status": "unfinished"},
+    {"name": "checkSystem", "describe": "ทำการ....", "deadline": "2025-08-24", "job_id": "1240", "status": "unfinished"}
+]
+
 def clear_terminal():
     if os.name == 'nt':
         os.system('cls')
@@ -16,18 +21,20 @@ def clear_terminal():
 
 
 class Job():
-    def __init__(self,name, describe ,deadline ,job_id = str(uuid.uuid4().int)[:4]):
+    def __init__(self,name, describe ,deadline ,job_id = str(uuid.uuid4().int)[:4],job_status = "unfinished"):
         self.name = name
         self.describe = describe
         self.deadline = deadline
         self.job_id = job_id
+        self.job_status = job_status
         pass
     def create_job(self):
         job_list.append({
         "name": self.name,
         "describe": self.describe,
         "deadline": self.deadline,
-        "job_id": self.job_id
+        "job_id": self.job_id,
+        "status": self.job_status
     })
 
 # แสดงผล
@@ -89,30 +96,43 @@ while state == True:
     print("1. Add Task ")
     print("2. Delete Task")
     print("3. Search Task")
+    print("4. Change status")
+    print("5. Save")
+    print("6. Load")
     print("99. Exit")
     menu_selector = int(input("Please Select a menu: "))
     if menu_selector == 1:
         print("you selected")
-        job_name = str(input("Please insert your job name :"))
+        while True:
+            job_name = str(input("Please insert your job name :"))
+            if job_name:
+                print(f"You entered: {job_name}")
+                break
+            else:
+                print("Input cannot be empty. Please try again.")
+        
         job_describe = str(input("Please describe your job :"))
-        date_string = input("Enter a date in YYYY-MM-DD format: ")
-        try:
-            year, month, day = map(int, date_string.split('-'))
-            user_date = dt.date(year, month, day)
-            print(f"You entered: {user_date}")
+        while True:
+            date_string = input("Enter a date in YYYY-MM-DD format: ")
+
+            try:
+                year, month, day = map(int, date_string.split('-'))
+                user_date = dt.date(year, month, day)
+                break
         
             
-        except ValueError:
-            print("Invalid date format. Please use YYYY-MM-DD.")
+            except ValueError:
+                print("Invalid date format. Please use YYYY-MM-DD.")
 
-        job = Job(job_name, job_describe, user_date)
+        job = Job(job_name, job_describe, str(user_date))
         job.create_job()
         clear_terminal()
-        display_list()
+        # display_list()
         
     elif menu_selector == 2:
-        job_id_to_remove = int(input("Please enter your job ID: "))
+        job_id_to_remove = input("Please enter your job ID: ")
         job_list = [i for i in job_list if i['job_id'] != job_id_to_remove]
+
         clear_terminal()
         display_list()
 
@@ -120,7 +140,7 @@ while state == True:
 
     elif menu_selector == 3:
         print("1. Search by keyword")
-        print("2. Search bu deadline")
+        print("2. Search by deadline")
         search_choice = int(input("Please select choice 1-2 : "))
 
         if search_choice == 1:
@@ -136,7 +156,7 @@ while state == True:
         else:
             print("Wrong input!!")
             found_jobs = []
-        
+        clear_terminal()
         print(f"\nพบงานทั้งหมด {len(found_jobs)} รายการ:")
         for job in found_jobs:
             print(job)
@@ -148,6 +168,40 @@ while state == True:
                 pass
             else:
                 state = False
+    elif menu_selector == 4:
+        job_id_to_change = input("Enter job ID to change status: ")
+
+        found = False
+        for job in job_list:
+            if job["job_id"] == job_id_to_change:
+                found = True
+                # toggle
+                if job["status"] == "unfinished":
+                    job["status"] = "finished"
+                    print(f"Status changed: unfinished → finished")
+                else:
+                    job["status"] = "unfinished"
+                    print(f"Status changed: finished → unfinished")
+                break
+
+        if not found:
+            print("Job ID not found!")
+
+        time.sleep(2)
+
+    elif menu_selector == 5:
+        with open('data.json', 'w') as f:
+            json.dump(job_list, f, indent = 4)
+        print("Saving...")
+        time.sleep(3)
+    elif menu_selector == 6:
+        with open('data.json', 'r') as f:
+            job_list = json.load(f)
+        print("Loading...")
+        time.sleep(3)
+
+
+
         
 
     elif menu_selector == 99:
